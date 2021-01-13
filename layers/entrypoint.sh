02286e8e9ca273deefdb587bf8d7ee0c0b687494
Matti Rantakömi <matti@rantakomi.fi>
2021-01-13 15:56:44 +0000

    fiksattu entrypoint

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -6,12 +6,23 @@ start(){
 }
 
 stop(){
+    echo "Stopping nginx"
     /usr/sbin/nginx -s stop
+    echo "Stopping php-fpm"
     kill $(cat /run/php8.0-fpm.pid)
     sleep 5
+    echo "Killing php-fpm"
     kill -9 $(cat /run/php8.0-fpm.pid)
+    exit 0
 }
 
-trap stop SIGINT SIGQUIT SIGHUP SIGTERM
+trap "echo TERM" TERM
+trap "echo HUP" HUP
+trap "echo INT" INT
+trap "echo QUIT" QUIT
+trap "echo USR1" USR1
+trap "echo USR2" USR2
+
+trap stop TERM HUP INT QUIT USR1 USR2
 
 start
