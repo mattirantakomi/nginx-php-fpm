cbe29072b4ab826ee6cc5b8984b7d42fca07bf1f
Matti Rantakömi <matti@rantakomi.fi>
2021-01-13 15:47:26 +0000

    fiksailua

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -6,8 +6,10 @@ start(){
 }
 
 stop(){
+    /usr/sbin/nginx -s stop
     kill $(cat /run/php8.0-fpm.pid)
-    kill $(cat /run/nginx.pid)
+    sleep 5
+    kill -9 $(cat /run/php8.0-fpm.pid)
 }
 
 trap stop SIGINT SIGQUIT SIGHUP SIGTERM
